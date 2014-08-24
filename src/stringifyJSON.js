c9c2658c931dfb958ef191997d1a722f3549377b
Greg Ferrell <gregerg@gmail.com>
2014-08-24 15:30:41 -0500

    completed stringifyJSON() function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,62 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  
+  var output = "";
+  if (typeof(obj) === "object") {
+    if(!obj) { return "null" }
+    
+    var isObjectLiteral = ( Object.getPrototypeOf(obj) === Object.getPrototypeOf({}) );
+  
+    
+    if (isObjectLiteral) {
+      output += "{";
+      for (var key in obj) {
+        var item = stringifyJSON(obj[key]);
+        if (!(typeof item === 'undefined')) {
+          output += "\""+key+"\":" + stringifyJSON(obj[key]) + ",";
+        }
+      }
+      // remove extra comma
+      if (output.length>1) {
+        output = output.slice(0,output.length-1);
+      }
+      output += "}";
+      return output;
+    } 
+  
+    else if (Array.isArray(obj)) {
+      output += "["
+      obj.forEach(function(item) {
+        var value = stringifyJSON(item);
+        if (!(typeof item === 'undefined')) {
+          output += stringifyJSON(item) + ",";
+        }
+      });
+      // remove extra comma
+      if (output.length>1) {
+        output = output.slice(0,output.length-1);
+      }
+      output += "]"
+      return output;
+    }
+  }
+  
+  // not object literal or array
+  switch (typeof obj) {
+    case "string": 
+      output = "\"" + obj + "\"";
+      break;
+    case "undefined":
+      output = undefined;
+      break;
+    case "function":
+      output = undefined;
+      break;
+    default:
+      output = obj.toString();
+  }
+  
+  return output;
 };
